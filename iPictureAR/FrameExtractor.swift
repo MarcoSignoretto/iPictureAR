bca910bb21a2fcb78445ac53e5aa22b99f40f719
Marco Signoretto <marco.signoretto.dev@gmail.com>
2019-11-16 20:44:01 +0400

    created frame extractor

---
+++ iPictureAR/FrameExtractor.swift
@@ -0,0 +1,87 @@
+//
+//  FrameExtractor.swift
+//  iPictureAR
+//
+//  Created by Marco Signoretto on 16/11/2019.
+//  Copyright © 2019 Marco Signoretto. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+class FrameExtractor: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate{
+    private let captureSession = AVCaptureSession()
+    private let sessionQueue = DispatchQueue(label: "session queue")
+    
+    private let position = AVCaptureDevice.Position.back
+    private let quality = AVCaptureSession.Preset.medium
+    
+    private var permissionGranted = false
+    
+    override init() {
+        super.init()
+        checkPermission()
+        
+        sessionQueue.async { [unowned self] in
+            self.configureSession()
+            self.captureSession.startRunning()
+            print("Session started")
+        }
+    
+    }
+    
+    private func checkPermission(){
+        switch AVCaptureDevice.authorizationStatus(for: AVMediaType.video) {
+            case .authorized:
+            // The user has explicitly granted permission for media capture
+                permissionGranted = true
+                print("Permission already granted")
+                break
+            
+            case .notDetermined:
+                requestPermission()
+                print("Permission requested")
+                break
+            
+            default:
+            // The user has denied permission
+                permissionGranted = false
+                print("Permission NOT granted")
+                break
+        }
+    }
+    
+    private func requestPermission(){
+        sessionQueue.suspend()
+        // The user has not yet granted or denied permission
+            AVCaptureDevice.requestAccess(for: AVMediaType.video) { [unowned self] granted in
+                self.permissionGranted = granted
+                self.sessionQueue.resume()
+            }
+    }
+    
+    private func configureSession(){
+        guard permissionGranted else { return }
+        captureSession.sessionPreset = quality
+        guard let captureDevice = selectCaptureDevice() else { return }
+        guard let captureDeviceInput = try? AVCaptureDeviceInput(device: captureDevice) else { return }
+        guard captureSession.canAddInput(captureDeviceInput) else { return }
+        captureSession.addInput(captureDeviceInput)
+        let videoOutput = AVCaptureVideoDataOutput()
+        videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "sample buffer"))
+        guard captureSession.canAddOutput(videoOutput) else { return }
+        captureSession.addOutput(videoOutput)
+        print("Setup Done")
+    }
+    
+    private func selectCaptureDevice() -> AVCaptureDevice? {
+        return AVCaptureDevice.devices().filter {
+            ($0 as AnyObject).hasMediaType(AVMediaType.video) &&
+            ($0 as AnyObject).position == position
+        }.first
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        print("Got a frame!")
+    }
+}
--- iPictureAR/SceneDelegate.swift
+++ iPictureAR/SceneDelegate.swift
@@ -8,18 +8,27 @@
 
 import UIKit
 import SwiftUI
+import AVFoundation
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
-
+    var frameExtractor: FrameExtractor?
+    
+    var captureSession:AVCaptureSession = AVCaptureSession()
+    var videoPreviewLayer:AVCaptureVideoPreviewLayer?
+    var qrCodeFrameView:UIView?
+    
+//    var frameExtractor : FrameExtractor
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
         // Create the SwiftUI view that provides the window contents.
+        
+        
         let contentView = ContentView()
 
         // Use a UIHostingController as window root view controller.
@@ -28,7 +37,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             window.rootViewController = UIHostingController(rootView: contentView)
             self.window = window
             window.makeKeyAndVisible()
+            
+            
         }
+        
+        self.frameExtractor = FrameExtractor()
         
         print("\(OpenCVWrapper.openCVVersionString())")
         print("\(IPictureARWrapper.myPrintNative())")
@@ -61,7 +74,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
 }
 
